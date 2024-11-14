4ea5a005a9e3ad54f2a20e13cd0d81a3a3b1d145
Victor2022246 <2022246@student.cct.ie>
2024-11-14 18:08:24 +0000

    start coding

---
+++ contracts/hospital.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+//Define the hospital contract 
+contract Hospital{
+    //Variable to store the administrator address
+address public administrator;
+//keeps traking of the hospital administration balance within the contract
+uint public adminBalance;
+
+//define the structure of the apoitments menu;
+struct servicesMenu{
+
+    string serviceName; // name of the service done by the hospital
+    uint price;  //price for the service 
+    string doctorName;
+    uint date;
+}
+struct Patient{    // structure for creationm of a Patient 
+
+ string patientName;
+ string medHistory;
+ uint age;
+ bool registered; // bollean to test if the patient is registered before making and appointment 
+}
+
+struct Appointment { // structure the appointments schedule  
+        address patient;
+        string patientName;
+        uint256 amount;
+        string treatmentDetails;
+        uint serviceId;
+        uint date;
+        string doctor;}
